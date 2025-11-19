7a0bae4d65be74bd0c3bad902676cc1d2f5cbc66
vladcalin-edw <calin.vlad@eaudeweb.ro>
2025-11-19 15:05:45 +0200

    fix typo

--- test/tests/plone-relstorage/run.sh
+++ test/tests/plone-relstorage/run.sh
@@ -21,42 +21,8 @@ pid="$(docker run -d --name "$pname" --link=$zname:db -e RELSTORAGE_DSN="dbname=
 trap "docker rm -vf $pid $zid > /dev/null" EXIT
 
 get() {
-	docker run --rm -i \
-		--link "$pname":plone \
-		--entrypoint /app/bin/python \
-		"$image" \
-		-c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
-}
-
-get_auth() {
-	docker run --rm -i \
-		--link "$pname":plone \
-		--entrypoint /app/bin/python \
-		"$image" \
-		-c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
-}
-
-. "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
-
-#!/bin/bash
-set -eo pipefail
-
-dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
-
-image="$1"
-
-PLONE_TEST_SLEEP=5
-PLONE_TEST_TRIES=20
-
-vname="/tmp/plone-data-$RANDOM-$RANDOM"
-cname="plone-container-$RANDOM-$RANDOM"
-vid="$(mkdir -p $vname)"
-cid="$(docker run -d --user=$(id -u) -v $vname:/data --name $cname $image)"
-trap "docker rm -vf $cid > /dev/null; rm -rf $vname > /dev/null" EXIT
-
-get() {
         docker run --rm -i \
-                --link "$cname":plone \
+                --link "$pname":plone \
                 --entrypoint /app/bin/python \
                 "$image" \
                 -c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
@@ -64,12 +30,11 @@ get() {
 
 get_auth() {
         docker run --rm -i \
-                --link "$cname":plone \
+                --link "$pname":plone \
                 --entrypoint /app/bin/python \
                 "$image" \
                 -c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
 }
-
 
 . "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
 
@@ -77,7 +42,7 @@ get_auth() {
 [[ "$(get 'http://plone:8080')" == *"Welcome to Plone!"* ]]
 
 # Create a Plone site (classic)
-[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"plone-overview.min.js"* ]]
+[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"Welcome to Plone!"* ]]
 
 # Create a Plone site (volto)
-[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=volto' "$(echo -n 'admin:admin' | base64)")" == *"plone-overview.min.js"* ]]
+[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=volto' "$(echo -n 'admin:admin' | base64)")" == *"Welcome to Plone!"* ]]
