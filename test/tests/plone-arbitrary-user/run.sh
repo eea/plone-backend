3f90a49237e2aeff26f950844606bdbf472eb8e7
vladcalin-edw <calin.vlad@eaudeweb.ro>
2025-11-19 14:59:20 +0200

    updated tests for 6.1.3

--- test/tests/plone-arbitrary-user/run.sh
+++ test/tests/plone-arbitrary-user/run.sh
@@ -15,19 +15,19 @@ cid="$(docker run -d --user=$(id -u) -v $vname:/data --name $cname $image)"
 trap "docker rm -vf $cid > /dev/null; rm -rf $vname > /dev/null" EXIT
 
 get() {
-	docker run --rm -i \
-		--link "$cname":plone \
-		--entrypoint /app/bin/python \
-		"$image" \
-		-c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
+        docker run --rm -i \
+                --link "$cname":plone \
+                --entrypoint /app/bin/python \
+                "$image" \
+                -c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
 }
 
 get_auth() {
-	docker run --rm -i \
-		--link "$cname":plone \
-		--entrypoint /app/bin/python \
-		"$image" \
-		-c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
+        docker run --rm -i \
+                --link "$cname":plone \
+                --entrypoint /app/bin/python \
+                "$image" \
+                -c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
 }
 
 
@@ -36,5 +36,8 @@ get_auth() {
 # Plone is up and running
 [[ "$(get 'http://plone:8080')" == *"Welcome to Plone!"* ]]
 
-# Create a Plone site
-#[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"plone-overview.min.js"* ]]
+# Create a Plone site (classic)
+[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"plone-overview.min.js"* ]]
+
+# Create a Plone site (volto)
+[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=volto' "$(echo -n 'admin:admin' | base64)")" == *"plone-overview.min.js"* ]]
--- test/tests/plone-relstorage/run.sh
+++ test/tests/plone-relstorage/run.sh
@@ -38,8 +38,46 @@ get_auth() {
 
 . "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
 
+#!/bin/bash
+set -eo pipefail
+
+dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+image="$1"
+
+PLONE_TEST_SLEEP=5
+PLONE_TEST_TRIES=20
+
+vname="/tmp/plone-data-$RANDOM-$RANDOM"
+cname="plone-container-$RANDOM-$RANDOM"
+vid="$(mkdir -p $vname)"
+cid="$(docker run -d --user=$(id -u) -v $vname:/data --name $cname $image)"
+trap "docker rm -vf $cid > /dev/null; rm -rf $vname > /dev/null" EXIT
+
+get() {
+        docker run --rm -i \
+                --link "$cname":plone \
+                --entrypoint /app/bin/python \
+                "$image" \
+                -c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
+}
+
+get_auth() {
+        docker run --rm -i \
+                --link "$cname":plone \
+                --entrypoint /app/bin/python \
+                "$image" \
+                -c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
+}
+
+
+. "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
+
 # Plone is up and running
 [[ "$(get 'http://plone:8080')" == *"Welcome to Plone!"* ]]
 
-# Create a Plone site
-#[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"Create a Plone site (Classic UI)"* ]]
+# Create a Plone site (classic)
+[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"plone-overview.min.js"* ]]
+
+# Create a Plone site (volto)
+[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=volto' "$(echo -n 'admin:admin' | base64)")" == *"plone-overview.min.js"* ]]
