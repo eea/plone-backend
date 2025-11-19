e9884dec97f6ec1175052ca460927b0d01bff824
Calin Vlad <calin.vlad@eaudeweb.ro>
2025-11-19 16:27:46 +0200

    test: updated tests for 6.1.3 - refs #288125

--- test/tests/plone-relstorage/run.sh
+++ test/tests/plone-relstorage/run.sh
@@ -21,19 +21,19 @@ pid="$(docker run -d --name "$pname" --link=$zname:db -e RELSTORAGE_DSN="dbname=
 trap "docker rm -vf $pid $zid > /dev/null" EXIT
 
 get() {
-	docker run --rm -i \
-		--link "$pname":plone \
-		--entrypoint /app/bin/python \
-		"$image" \
-		-c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
+        docker run --rm -i \
+                --link "$pname":plone \
+                --entrypoint /app/bin/python \
+                "$image" \
+                -c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
 }
 
 get_auth() {
-	docker run --rm -i \
-		--link "$pname":plone \
-		--entrypoint /app/bin/python \
-		"$image" \
-		-c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
+        docker run --rm -i \
+                --link "$pname":plone \
+                --entrypoint /app/bin/python \
+                "$image" \
+                -c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
 }
 
 . "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
@@ -41,5 +41,8 @@ get_auth() {
 # Plone is up and running
 [[ "$(get 'http://plone:8080')" == *"Welcome to Plone!"* ]]
 
-# Create a Plone site
-#[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"Create a Plone site (Classic UI)"* ]]
+# Create a Plone site (classic)
+[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"Welcome to Plone!"* ]]
+
+# Create a Plone site (volto)
+[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=volto' "$(echo -n 'admin:admin' | base64)")" == *"Welcome to Plone!"* ]]
